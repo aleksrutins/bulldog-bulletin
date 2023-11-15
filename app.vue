a2c7e60c3db673b5bbcfade1ba0540b47465dadd
Aleks Rutins <aleks@rutins.com>
2023-11-15 07:45:53 -0500

    stuff

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
-<script setup>
-const { data: articles } = await useArticles();
-</script>
-
 <template>
-  <div>
-    <div v-for="article of articles">
-      <h1>{{ article.title }}</h1>
-    </div>
-  </div>
+  <RouterView />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { getItems } = useDirectusItems();
+const articles = await getItems({ collection: 'articles' })
+</script>
+<template>
+    <div>
+        <div v-for="article of articles">
+            <h1>{{ article.title }}</h1>
+        </div>
+    </div>
+</template>
