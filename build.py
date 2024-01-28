4f23875a10855fd963fc43fe6517771d99d9d5d1
Aleks Rutins <aleks@rutins.com>
2024-01-28 12:00:14 -0500

    formatting

--- build.py
+++ build.py
@@ -8,26 +8,35 @@ compile_template('index', '/', data)
 
 tags_compiled = set()
 for article in data['articles']:
-    compile_template('article', f'/articles/{article['id']}', data, article=article)
-    
+    compile_template(
+        'article',
+        f'/articles/{article['id']}',
+        data, article=article
+    )
+
     for tag in article['categories']:
         if tag not in tags_compiled:
             tags_compiled.add(tag)
-            compile_template('category', f'/c/{slugify(tag)}', data, 
+            compile_template('category', f'/c/{slugify(tag)}', data,
                              tag=tag,
-                             filtered_articles= 
-                                [
-                                    article for article in data['articles']
-                                    if tag in article['categories']
-                                ]
+                             filtered_articles=[
+                                article for article in data['articles']
+                                if tag in article['categories']
+                             ]
                              )
 
 for issue in data['issues']:
     issue_articles = [
         article for article in data['articles']
-        if article['issue'] is not None and article['issue']['id'] == issue['id']
+        if
+        article['issue'] is not None
+        and article['issue']['id'] == issue['id']
     ]
 
-    compile_template('issue', f'/issues/{issue['id']}', data, issue=issue, issue_articles=issue_articles)
+    compile_template(
+        'issue',
+        f'/issues/{issue['id']}',
+        data, issue=issue, issue_articles=issue_articles
+    )
 
 write_sitemap()
